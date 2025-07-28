d376d18ca2fb78e28911f9f6ccd2783f59461695
Ronaldo Arley Tambillo Mamani <rmamani@vsb.net.br>
2025-07-28 11:41:24 -0300

    Adicionando no frontend os services de Cidade e Estado, e conseguindo consumir a API Laravel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Se seu app.component.html usar diretivas comuns
+import { RouterOutlet } from '@angular/router'; // Se você estiver usando o roteador
+import { EstadosComponent } from './estados/estados.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // Seu componente raiz também deve ser standalone
+  imports: [CommonModule, EstadosComponent], // Adicione o EstadosComponent aqui!
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clinica-infantil-frontend';
+  title = 'frontend-clinica';
 }
